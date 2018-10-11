d442b0103670afc370d0a83e1ecc9b83f36b0a9c
sravani <sravani.ravula@wsu.edu>
2018-10-11 15:45:23 -0700

    testCorrectness

---
+++ testCorrectness.js
@@ -0,0 +1,29 @@
+/* eslint-env mocha, chai */
+/* global chai, primeGen, cumulativeSum, maxPrimeSum */
+
+describe('Test for Correction', function () {
+  describe('primGen', function () {
+    it('primGen(10) => [2, 3, 5, 7]', function () {
+      chai.expect(primGen(10)).to.have.members([2, 3, 5, 7]);
+    });
+    it('primeGen(20) => [2, 3, 5, 7, 11, 13, 17, 19]', function () {
+      chai.expect(primGen(20)).to.have.members([2, 3, 5, 7, 11, 13, 17, 19]);
+    });
+  });
+  describe('cumulativeSum', function () {
+    it('cumulativeSum([1, 2, 3, 4]) => [1, 3, 6, 10]', function () {
+      chai.expect(cumulativeSum([1, 2, 3, 4])).to.have.members([1, 3, 6, 10]);
+    });
+    it('cumulativeSum([10, 11, 12, 13, 14]) => [10, 21, 33, 46, 60]', function () {
+      chai.expect(cumulativeSum([10, 11, 12, 13, 14])).to.have.members([10, 21, 33, 46, 60]);
+    });
+  });
+  describe('maxPrimeSum', function () {
+    it('maxPrimeSum(100) => [41, 6]', function () {
+      chai.expect(maxPrimeSum(100)).to.have.members([41, 6]);
+    });
+    it('maxPrimeSum(1000) => [953, 21]', function () {
+      chai.expect(maxPrimeSum(1000)).to.have.members([953, 21]);
+    });
+  });
+});
